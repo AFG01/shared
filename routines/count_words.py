212fe5f7cf616afe9d19ef4ee57351e40b190cc1
Irfan <m.irfanphy@gmail.com>
2015-11-04 14:43:43 +0000

    frequency of first letter of a word

--- routines/count_words.py
+++ routines/count_words.py
@@ -10,3 +10,12 @@ def count(string):
     word_list = str.split(pfs)
     count = len(word_list)
     return count, word_list
+
+def freq_letter(letter, word_list, count):
+    occur = 0
+    for i in word_list:
+        if i[0] == letter:
+            occur += 1
+    frequency = occur/count
+    return frequency
+        
